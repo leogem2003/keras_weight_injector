5b7bb9c0f60ab5c52133104bc53c81a42f08a233
Leonardo Gementi <leogem05@gmail.com>
2024-10-23 18:10:20 +0200

    created independent module

---
+++ model_conversion/model_conversion/utils.py
@@ -0,0 +1,21 @@
+SUPPORTED_DATASETS = ["CIFAR10", "CIFAR100", "GTSRB", "IMAGENET"]
+
+SUPPORTED_MODELS = [
+    "ResNet18",
+    "ResNet20",
+    "ResNet32",
+    "ResNet44",
+    "ResNet56",
+    "ResNet110",
+    "ResNet1202",
+    "DenseNet121",
+    "DenseNet161",
+    "DenseNet169",
+    "GoogLeNet",
+    "MobileNetV2",
+    "InceptionV3",
+    "Vgg11_bn",
+    "Vgg13_bn",
+    "Vgg16_bn",
+    "Vgg19_bn",
+]
