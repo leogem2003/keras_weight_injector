3a5ae6267f9f5344e4854b2c8329c1b04566699c
Leonardo Gementi <leogem05@gmail.com>
2024-12-05 16:06:26 +0100

    changed testing

--- tf_injector/testing/output_consistency.py
+++ tf_injector/testing/output_consistency.py
@@ -6,38 +6,41 @@ import sys
 
 
 def get_dirs(dir):
-    yield from (d.__fspath__() for d in os.scandir(dir) if d.is_dir())
+    return (d.__fspath__() for d in os.scandir(dir) if d.is_dir())
 
 
-def create_batches(base_dir):
+def test_all(base_dir):
     datasets = get_dirs(base_dir)
     models = {dataset: get_dirs(dataset) for dataset in datasets}
     for dt in models:
-        print("validating dataset", dt)
         for model in models[dt]:
-            print("\tvalidating model", model)
-            output_files = chain.from_iterable(
-                (glob.glob(dir + "/*.npy") for dir in get_dirs(model))
-            )
-            clusters = {}
-            for file in output_files:
-                basename = os.path.basename(file)
-                if not basename in clusters:
-                    clusters[basename] = []
-                clusters[basename].append(file)
+            test(model)
 
-            for cl, files in sorted(clusters.items()):
-                print(f"\t\tvalidating {cl} using reference {files[0]}")
-                if len(files) > 1:
-                    ref = np.load(files[0])
-                    for other in files[1:]:
-                        comp = np.load(other)
-                        try:
-                            assert (ref == comp).all()
-                            print(f"\t\t\t{other:>32} OK")
-                        except AssertionError:
-                            print(f">>>>\t\t{other:>32} AssertionError!")
+
+def test(model):
+    output_files = chain.from_iterable(
+        (glob.glob(dir + "/*.npy") for dir in get_dirs(model))
+    )
+    clusters = {}
+    for file in output_files:
+        basename = os.path.basename(file)
+        if not basename in clusters:
+            clusters[basename] = []
+        clusters[basename].append(file)
+
+    for _, files in sorted(clusters.items()):
+        if len(files) > 1:
+            ref = np.load(files[0])
+            for other in files[1:]:
+                comp = np.load(other)
+                try:
+                    assert (ref == comp).all()
+                except AssertionError:
+                    distance = np.sqrt(((ref - comp) ** 2).sum())
+                    print(
+                        f"AssertionError: {files[0]} vs {other} differ with distance {distance:.6f}"
+                    )
 
 
 if __name__ == "__main__":
-    create_batches(sys.argv[1])
+    test(sys.argv[1])
--- tf_injector/testing/reports_consistency.py
+++ tf_injector/testing/reports_consistency.py
@@ -2,7 +2,6 @@ import glob
 import os
 import sys
 import csv
-import itertools
 
 
 def get_dirs(dir):
@@ -18,39 +17,39 @@ def validate_reports(base_dir):
     datasets = get_dirs(base_dir)
     models = {dataset: get_dirs(dataset) for dataset in datasets}
     for dt in models:
-        print("validating dataset", dt)
         for model in models[dt]:
-            print(f"    validating model {model}")
-            csvs = sorted(glob.glob(model + "/*.csv"))
-            injs = []
-            all_data = {}
-            for file in csvs:
-                with open(file, "r") as f:
-                    reader = csv.reader(f)
-                    all_data[file] = []
-                    for row in reader:
-                        if row[:4] not in injs:
-                            injs.append(row[:4])
-                        all_data[file].append(row)
+            validate_model(model)
 
-            for inj in injs:
-                selected = []
-                for file, finjs in all_data.items():
-                    for fi in finjs:
-                        if inj == fi[:4]:
-                            selected.append((file, fi))
-                            break
-                first = selected[0][1]
-                print(f"        validating {selected[0][0]}#{first[0]}")
-                for file, i in selected:
-                    try:
-                        assert i == first
-                    except AssertionError:
-                        print(
-                            f"        Assertion Error in file {file}\n\
->>>>>>>> {first[4:]} vs {i[4:]}"
-                        )
+
+def validate_model(model):
+    csvs = sorted(glob.glob(model + "/*.csv"))
+    injs = []
+    all_data = {}
+    for file in csvs:
+        with open(file, "r") as f:
+            reader = csv.reader(f)
+            all_data[file] = []
+            for row in reader:
+                if row[:4] not in injs:
+                    injs.append(row[:4])
+                all_data[file].append(row)
+    for inj in injs:
+        selected = []
+        for file, finjs in all_data.items():
+            for fi in finjs:
+                if inj == fi[:4]:
+                    selected.append((file, fi))
+                    break
+        first = selected[0][1]
+        for file, i in selected:
+            try:
+                assert i == first
+            except AssertionError:
+                print(
+                    f"{model}: Assertion Error {selected[0][0]} vs {file}:\
+{first[4:]} vs {i[4:]}"
+                )
 
 
 if __name__ == "__main__":
-    validate_reports(sys.argv[1])
+    validate_model(sys.argv[1])
