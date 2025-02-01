8d060db88fc6f55960dfdf540b6bf1dd2e9011b8
Leonardo Gementi <leogem05@gmail.com>
2025-02-01 14:58:56 +0100

    separated conversion tools

--- legacy_benchmark_models/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m benchmark_models.injector.tf_weight_injection -n DenseNet121 -d CIFAR100 --fault-list fault_lists/CIFAR100/DenseNet121_CIFAR100_fault_list.csv --sort-tf-layers -b 512
--- model_conversion/model_conversion/__main__.py
+++
@@ -1,4 +0,0 @@
-from model_conversion.converter import main, parse_args
-
-if __name__ == "__main__":
-    main(parse_args())
